6ed6123c57d47c91f0fd15b51a09217ae8d70737
Nanda <nanda.sankaran@gmail.com>
2025-08-18 17:00:54 +1000

    Update default bounds for EPSG:3031.

--- src/Models/defaultCrsDefinitions.ts
+++ src/Models/defaultCrsDefinitions.ts
@@ -4,32 +4,12 @@ export const defaultCrsDefinitions = [
     name: "Web Mercator"
   },
   {
-    // preview map requires to be in zoom=0
     crs: "EPSG:3031",
     name: "Antartica",
     projectedBounds: {
-      min: {
-        x: -4194304,
-        y: -4194304
-      },
-      max: {
-        x: 4194304,
-        y: 4194304
-      }
+      min: { x: -2668275.0, y: -2294665.0 },
+      max: { x: 2813725.0, y: 2362335.0 }
     },
     clipLayersToRectangle: false
   }
-  // {
-  //   crs: "EPSG:28355",
-  //   projectedBounds: {
-  //     min: {
-  //       x: -6445257.39,
-  //       y: -2068741.81
-  //     },
-  //     max: {
-  //       x: 3502694.93,
-  //       y: 9063625.73
-  //     }
-  //   }
-  // }
 ];
